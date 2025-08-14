c3f2b4ac129ad70a1a4f35f764e601d9915a437d
Joel-Ferrer-Mello <femsoft.fdm@gmail.com>
2025-08-14 12:47:00 +0100

    feature(nodejs/class17: finished this JS class about use of the HELMET and CSRF

--- App_nodeJS/class17/server.js
+++ App_nodeJS/class17/server.js
@@ -1,28 +1,23 @@
 require('dotenv').config();
-
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
-
 mongoose.connect( process.env.CONNECTIONSTRING )
   .then(() => {
     app.emit('pronto');
   })
   .catch(e => console.log(e));
-
-//const session = require('express-session');
-//const MongoStore = require('connect-mongo')(session);
 const session = require('express-session');
 const MongoStore = require('connect-mongo');
-
 const flash = require('connect-flash');
-
 const routes = require('./routes');
 const path = require('path');
-const { middlewareGlobal } = require('./src/middlewares/middleware');
+const helmet = require('helmet');
+const csrf = require('csurf');
+const { middlewareGlobal, checkCsrfError, csrfMiddleware } = require('./src/middlewares/middleware');
 
+app.use(helmet());
 app.use(express.urlencoded({ extended: true }));
-
 app.use(express.static(path.resolve(__dirname, 'public')));
 
 const sessionOptions = session({
@@ -41,8 +36,11 @@ app.use(flash());
 app.set('views', path.resolve(__dirname, 'src', 'views'));
 app.set('view engine', 'ejs');
 
+app.use(csrf());
 // Nossos próprios middlewares
 app.use(middlewareGlobal);
+app.use(checkCsrfError);
+app.use(csrfMiddleware);
 app.use(routes);
 
 app.on('pronto', () => {
--- App_nodeJS/class17/src/middlewares/middleware.js
+++ App_nodeJS/class17/src/middlewares/middleware.js
@@ -7,3 +7,14 @@ exports.otherMiddleware = (req, res, next) => {
     console.log('I am your other middleware');
     next();
 };
+
+exports.checkCsrfError = (err, req, res, next) => {
+    if(err && 'EBADCSRFTOKEN' === err.code) {
+        return res.render('404');
+    }
+};
+
+exports.csrfMiddleware = (req, res, next) => {
+    res.locals.csrfToken = req.csrfToken();
+    next();
+};

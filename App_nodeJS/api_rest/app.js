01021e0f1a2b4528198f2f8479393f2ccb020b57
Joel Mello JS <mellojoel.hr@gmail.com>
2025-09-19 09:49:13 +0100

    Feature(API REST): class 176 was finished

--- App_nodeJS/api_rest/app.js
+++ App_nodeJS/api_rest/app.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import homeRoutes from './src/routes/homeRoutes';
+import userRoutes from './src/routes/userRoutes';
 import './src/database';
 import dotenv from 'dotenv';
 dotenv.config();
@@ -18,6 +19,7 @@ class App {
 
   routes() {
     this.app.use('/', homeRoutes);
+    this.app.use('/users/', userRoutes);
   }
 }
 

d5eee23eeff8320fea4e6e7403efb03a17dce3a5
Joel Mello JS <mellojoel.hr@gmail.com>
2025-11-14 18:46:44 +0000

    (RegExp): match and replace

--- JS/Regular-expressions/class02.js
+++ JS/Regular-expressions/class02.js
@@ -1,4 +1,9 @@
 const { text } = require('./base');
 const regExp1 = /rock/gi;
 
+console.log(text);
 console.log(text.match(regExp1));
+console.log(text.replace(/beatles/gi, 'Nirvana'));
+console.log(text.replace(/beatles/gi, function(input){
+  return ' ############### ' + input.toUpperCase() + ' ############### ';
+}));
